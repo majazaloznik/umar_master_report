4fc8d2d64168d247918a1f80b18e191d62b8fd28
majazaloznik <maja.zaloznik@gmail.com>
2023-07-12 16:54:02 +0200

    prva dva grafa za Mojco

---
+++ docs/children/html_61_realni_prihodek_v_gostinstvu.R
@@ -0,0 +1,37 @@
+# get data
+df <- read.csv2(here::here("data/047.csv"), encoding = "UTF-8")
+spl <- split(df, df$chart_no)
+# prepare data
+prep_l <- invisible(prep_multi_line(spl[[1]], con))
+
+purrr::reduce(prep_l$data_points, dplyr::left_join, by = c("period_id", "period")) %>%
+  dplyr::relocate( period) |>
+  select(-period_id) |>
+  as_tibble() -> data
+
+data |>
+  plot_ly(x = ~period, hovertemplate="%{x|%b-%Y} %{y:.2f}", width = 1000) |>
+  add_bars(y = ~`value.x`, name = "Skupaj gostinstvo",  color = I(umar_cols()[3])) |>
+  add_lines(y = ~`value.y`, name = "Gostinske nastanitvene dejavnosti",  color = I(umar_cols()[1])) |>
+  add_lines(y = ~`value`, name = "Dejavnost strežbe jedi in pija\u010d",  color = I(umar_cols()[2])) |>
+  layout(showlegend = TRUE,
+         autosize = F, margin = m,
+         font=list(family = "Myriad Pro"),
+         yaxis = list(title = list(text="Medletna rast, v %",
+                                   font = list(size =12)),
+                      fixedrange = FALSE),
+         xaxis = list(title = "",
+                      tickformatstops = list(
+                        list(dtickrange = list("M1", "M6"),
+                             value = "%b-%Y"),
+                        list(dtickrange = list("M6", NULL),
+                             value = "%Y"))),
+         title = list(text = paste("Posodobljeno:", prep_l$updated,
+                                   prep_l$transf_txt, "(Vir: SURS & prera\u010dun UMAR)"),
+                      font = list(size = 12),
+                      x = 0))|>
+  rangeslider(as.Date("2019-01-01"), max(data$period) + 30)|>
+  config(modeBarButtonsToAdd = list(dl_button))
+
+
+
